3512ee341dba72050f2815af74a40cde660c9989
Shoshtari <mr.malekey@gmail.com>
2022-03-11 18:21:36 +0330

    Registered 'main' app and Fixing templates

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from http.client import HTTPResponse
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def test(request):
+    var = {"word":"salam"}
+    return render(request, 'main/test.html', context=var)
